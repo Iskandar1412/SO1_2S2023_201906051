a39ab44731e49b2258bccd5a7a5175a9922d9142
Paco Silva <silvapaco810@gmail.com>
2023-08-26 09:41:18 -0600

    Ajustes (no agarra en el 22.04)

--- Tareas/Tarea_04/Modulo/modulo_ram.c
+++ Tareas/Tarea_04/Modulo/modulo_ram.c
@@ -1,67 +1,71 @@
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/hugetlb.h>
+#include <linux/mman.h>
+#include <linux/mmzone.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#include <linux/sysinfo.h>
+#include <linux/swap.h>
+#include <linux/vmstat.h>
+#include <linux/atomic.h>
+#include <linux/vmalloc.h>
+#include <asm/page.h>
+#include <asm/pgtable.h>
+#include <asm/uaccess.h>
 
-#define MODULE_NAME "modulo_ram"
-#define PROC_FILENAME "ram_info"
+#define FileProc "ram_201906051"
 
-static int meminfo_read_proc(char *page, char **start, off_t off,
-                             int count, int *eof, void *data)
-{
-    struct sysinfo i;
-    si_meminfo(&i);
+MODULE_AUTHOR("Juan Urbina");
+MODULE_DESCRIPTION("Sistemas Operativos 1");
+MODULE_LICENSE("GPL");
 
-    unsigned long total_ram = i.totalram * (PAGE_SIZE / 1024);
-    unsigned long ram_in_use = (i.totalram - i.freeram) * (PAGE_SIZE / 1024);
-    unsigned long ram_free = i.freeram * (PAGE_SIZE / 1024);
-    int ram_percentage = (int)((ram_in_use * 100) / total_ram);
+struct sysinfo i;
 
-    return sprintf(page,
-                   "{\n"
-                   "\"total_ram\": %lu,\n"
-                   "\"Ram_en_uso\": %lu,\n"
-                   "\"Ram_libre\": %lu,\n"
-                   "\"Porcentaje_en_uso\": %d\n"
-                   "}\n",
-                   total_ram, ram_in_use, ram_free, ram_percentage);
+unsigned long committed;
+unsigned long allowed;
+long cached;
+unsigned long pages[NR_LRU_LISTS];
+int lru;
+
+static int show_memory_stat(struct seq_file *f, void *v) {
+	si_meminfo(&i);
+
+	seq_printf(f, "{\n");
+	seq_printf(f, "\"total_ram\":" "%lu,\n", ((i.totalram) << (PAGE_SHIFT - 10))/1024);
+	seq_printf(f, "\"free_ram\":" "%lu,\n", ((i.freeram) << (PAGE_SHIFT - 10))/1024);
+	seq_printf(f, "\"used_ram\":" "%lu,\n", (((((i.totalram) << (PAGE_SHIFT - 10)) - ((i.freeram) << (PAGE_SHIFT -10))) * 4) / 1024));
+	seq_printf(f, "\"percent_ram\":" "%lu,\n", (((((i.totalram) << (PAGE_SHIFT - 10)) - ((i.freeram) << (PAGE_SHIFT -10))) * 4) / 1024) / 100);
+	seq_printf(f, "{\n");
+
+	return 0;	
 }
 
-static int ram_proc_show(struct seq_file *m, void *v) {
-    char buf[4096];
-    int len = meminfo_read_proc(buf, NULL, 0, sizeof(buf), NULL, NULL);
-    if (len > 0)
-        seq_write(m, buf, len);
-    return 0;
+static int meminfo_proc_open(struct inode *inode, struct file*file) {
+	return single_open(file, show_memory_stat, NULL);
 }
 
-static int ram_proc_open(struct inode *inode, struct file *file) {
-    return single_open(file, ram_proc_show, NULL);
-}
-
-static const struct proc_ops ram_proc_ops = {
-    .proc_open = ram_proc_open,
-    .proc_read = seq_read,
-    .proc_lseek = seq_lseek,
-    .proc_release = single_release,
+static const struct file_operations Meminfo_fops = {
+	.owner = THIS_MODULE,
+	.open = meminfo_proc_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+		.release = seq_release
 };
 
-static int __init ram_module_init(void) {
-    printk(KERN_INFO "ram_module: 201906051\n");
-
-    proc_create(PROC_FILENAME, 0, NULL, &ram_proc_ops);
-
-    return 0;
+static int __init start_function(void) {
+	printk(KERN_INFO "Cargando Modulo Ram");
+	proc_create(FileProc, 0777, NULL, &Meminfo_fops);
+		printk(KERN_INFO "Archivo creado: /proc/%s\n", FileProc);
+		return 0;
 }
 
-static void __exit ram_module_exit(void) {
-    remove_proc_entry(PROC_FILENAME, NULL);
-    printk(KERN_INFO "ram_module: Juan Urbina\n");
+static void __exit clean_function(void) {
+	printk(KERN_INFO "Modulo Eliminado");
+	remove_proc_entry(FileProc, NULL);
 }
 
-module_init(ram_module_init);
-module_exit(ram_module_exit);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Juan Urbina");
-MODULE_DESCRIPTION("RAM Info Module");
+module_init(start_function);
+module_exit(clean_function);
