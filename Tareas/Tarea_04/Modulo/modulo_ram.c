fe1af1d44579f0c3dfdfa309eb97d07007093162
Paco Silva <silvapaco810@gmail.com>
2023-08-26 01:58:32 -0600

    Ajustes

--- Tareas/Tarea_04/Modulo/modulo_ram.c
+++ Tareas/Tarea_04/Modulo/modulo_ram.c
@@ -3,24 +3,35 @@
 #include <linux/seq_file.h>
 #include <linux/sysinfo.h>
 
+#define MODULE_NAME "modulo_ram"
 #define PROC_FILENAME "ram_info"
 
-static int ram_proc_show(struct seq_file *m, void *v) {
-    struct sysinfo info;
-    si_meminfo(&info);
+static int meminfo_read_proc(char *page, char **start, off_t off,
+                             int count, int *eof, void *data)
+{
+    struct sysinfo i;
+    si_meminfo(&i);
 
-    unsigned long total_ram = info.totalram * (info.mem_unit / 1024);
-    unsigned long ram_in_use = (info.totalram - info.freeram) * (info.mem_unit / 1024);
-    unsigned long ram_free = info.freeram * (info.mem_unit / 1024);
+    unsigned long total_ram = i.totalram * (PAGE_SIZE / 1024);
+    unsigned long ram_in_use = (i.totalram - i.freeram) * (PAGE_SIZE / 1024);
+    unsigned long ram_free = i.freeram * (PAGE_SIZE / 1024);
     int ram_percentage = (int)((ram_in_use * 100) / total_ram);
 
-    seq_printf(m, "{\n");
-    seq_printf(m, "\"total_ram\": %lu,\n", total_ram);
-    seq_printf(m, "\"Ram_en_uso\": %lu,\n", ram_in_use);
-    seq_printf(m, "\"Ram_libre\": %lu,\n", ram_free);
-    seq_printf(m, "\"Porcentaje_en_uso\": %d\n", ram_percentage);
-    seq_printf(m, "}\n");
+    return sprintf(page,
+                   "{\n"
+                   "\"total_ram\": %lu,\n"
+                   "\"Ram_en_uso\": %lu,\n"
+                   "\"Ram_libre\": %lu,\n"
+                   "\"Porcentaje_en_uso\": %d\n"
+                   "}\n",
+                   total_ram, ram_in_use, ram_free, ram_percentage);
+}
 
+static int ram_proc_show(struct seq_file *m, void *v) {
+    char buf[4096];
+    int len = meminfo_read_proc(buf, NULL, 0, sizeof(buf), NULL, NULL);
+    if (len > 0)
+        seq_write(m, buf, len);
     return 0;
 }
 
@@ -28,7 +39,7 @@ static int ram_proc_open(struct inode *inode, struct file *file) {
     return single_open(file, ram_proc_show, NULL);
 }
 
-static const struct proc_ops ram_proc_fops = {
+static const struct proc_ops ram_proc_ops = {
     .proc_open = ram_proc_open,
     .proc_read = seq_read,
     .proc_lseek = seq_lseek,
@@ -38,7 +49,7 @@ static const struct proc_ops ram_proc_fops = {
 static int __init ram_module_init(void) {
     printk(KERN_INFO "ram_module: 201906051\n");
 
-    proc_create(PROC_FILENAME, 0, NULL, &ram_proc_fops);
+    proc_create(PROC_FILENAME, 0, NULL, &ram_proc_ops);
 
     return 0;
 }
