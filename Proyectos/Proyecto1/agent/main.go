4c9e1a6272a8e1010dcdbbd43589996c02c37706
Paco Silva <silvapaco810@gmail.com>
2023-09-11 11:54:56 -0600

    Archivo go

--- Proyectos/Proyecto1/agent/main.go
+++ Proyectos/Proyecto1/agent/main.go
@@ -6,6 +6,22 @@ import (
 	"os/exec"
 )
 
+type Procesos struct {
+	Proceso  string `json:"Proceso"`
+	PID      int    `json:"PID"`
+	UID      int    `json:"UID"`
+	Estado   string `json:"Estado"`
+	MemoriaV int    `json:"Memoria_virtual"`
+	MemoriaF int    `json:"Memoria_fisica"`
+}
+
+type InfoCPU struct {
+	NombreEquipo  string     `json:"Nombre_equipo"`
+	UsuarioActual string     `json:"Usuario_actual"`
+	Proc          []Procesos `json:"Procesos"`
+	UsoCPU        int        `json:"Uso_de_CPU"`
+}
+
 func main() {
 	http.HandleFunc("/kill", func(w http.ResponseWriter, r *http.Request) {
 		pid := r.FormValue("pid")
